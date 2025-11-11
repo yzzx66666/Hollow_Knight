e4618903980a666c33b8719351a7247eed07ea88
Laganerhhh <2424374510@qq.com>
2025-11-11 22:00:07 +0800

    做了一个小小的地图

---
+++ Assets/Scripts/Environment/Objects/PlantBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantBase : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //播放动画
+            animator.SetTrigger("interact");
+        }
+    }
+}
